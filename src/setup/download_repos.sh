f6539c7fd99ae8f05fd5591b95c394979df6acfd
Willem Meijer <w.meijer.5@student.rug.nl>
2022-03-18 09:47:59 +0100

    added https?

--- src/setup/download_repos.sh
+++ src/setup/download_repos.sh
@@ -9,7 +9,7 @@ echo -e "\n Cloning GFPG-LLMA"
 git clone --progress https://github.com/cofiiwu/GFPG-LLMA.git ./repositories/gfpg-llma
 
 echo -e "\nDownloading IBM Quest Data Generator"
-curl -o ./repositories/ibm_generator.zip http://www.philippe-fournier-viger.com/spmf/datasets/IBM_Quest_data_generator.zip
+curl -o ./repositories/ibm_generator.zip https://www.philippe-fournier-viger.com/spmf/datasets/IBM_Quest_data_generator.zip
 unzip ./repositories/ibm_generator.zip -d ./repositories/ibm_generator
 rm ./repositories/ibm_generator.zip
 

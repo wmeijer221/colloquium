8837a3802220db9b92f2cc3cc40db2a4705c6e6e
Willem Meijer <w.meijer.5@student.rug.nl>
2022-03-18 09:22:29 +0100

    Added GFPG-LLMA repository. Removed gss folder.

--- src/setup/download_repos.sh
+++ src/setup/download_repos.sh
@@ -1,8 +1,12 @@
 
+rm -r ./repositories
 mkdir ./repositories
 
 echo -e "\nCloning GMiner"
 git clone --progress https://github.com/coderbond007/GMiner.git ./repositories/gminer
+
+echo -e "\n Cloning GFPG-LLMA"
+git clone --progress https://github.com/cofiiwu/GFPG-LLMA.git ./repositories/gfpg-llma
 
 echo -e "\nDownloading IBM Quest Data Generator"
 curl -o ./repositories/ibm_generator.zip http://www.philippe-fournier-viger.com/spmf/datasets/IBM_Quest_data_generator.zip
